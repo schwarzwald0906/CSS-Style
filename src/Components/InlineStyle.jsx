4c0b55f6b221a7a08ea054e5d17d4e6d38c36d55
schwarzwald0906 <93188453+schwarzwald0906@users.noreply.github.com>
2022-08-03 22:26:51 +0900

    CSS

--- src/Components/InlineStyle.jsx
+++ src/Components/InlineStyle.jsx
@@ -1,8 +1,29 @@
 export const InlineStyle = () => {
+  const containerStyle = {
+    border: "solid 2px #392eff",
+    padding: "20px",
+    margin: "8px",
+    display: "flex",
+    justifyContent: "Space-around",
+    alignItems: "center"
+  };
+
+  const titleStyle = {
+    margin: 0,
+    color: "#3d84a8"
+  };
+
+  const buttonStyle = {
+    backGroundColor: "#abedd8",
+    border: "none",
+    padding: "8px",
+    borderRadius: "8px"
+  };
+
   return (
-    <div>
-      <p>-InlineStyle-</p>
-      <button>ボタン</button>
+    <div style={containerStyle}>
+      <p style={titleStyle}>-InlineStyle-</p>
+      <button style={buttonStyle}>Fight!!</button>
     </div>
   );
 };
